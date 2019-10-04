ea6dadac8f8d5ee4ff18f256857b0edf5b927535
BryantErwin <bryanterwin14@gmail.com>
2019-10-04 06:05:22 -0400

    Made mvp

---
+++ baseball/src/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App.js';
+import Display from './components/Display.js';
+import Dashboard from './components/Dashboard.js';
+
+test('App renders without crashing', () => {
+  render(<App />);
+});
+
+test('contains Balls and Strikes Count', () => {
+  const containerDisplay = render(<Display />);
+  containerDisplay.getByText(/balls/i);
+  containerDisplay.getByText(/strikes/i);
+});
+
+
+test('contains foul and hit button', () => {
+  const containerDash = render(<Dashboard />);
+  containerDash.getByText(/hit/i);
+  containerDash.getByText(/foul/i);
+});
